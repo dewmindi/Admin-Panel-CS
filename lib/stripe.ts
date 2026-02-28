731348a5954cf57068ed7afdb79916bd719acc60
dewmindi <udewmindi.ud@gmail.com>
2026-02-28 22:48:27 +0530

    Fix build error 1

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,10 @@
 import Stripe from "stripe"
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("STRIPE_SECRET_KEY is not set")
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2024-12-18.acacia" as Stripe.LatestApiVersion,
 })
 
